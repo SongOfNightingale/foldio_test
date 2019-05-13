f8694134c26e444c758439f6e067372e26135528
SongOfNightingale <crawling_91@mail.ru>
2019-05-13 15:40:57 +0200

    Add files via upload

---
+++ foldio.ts
@@ -0,0 +1,27 @@
+namespace foldio {
+       /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% blockId=device_foldio_leftEar block="On Left Ear touch" 
+    //%blockExternalInputs=false
+    //% shim=foldio::ltouch
+    export function lTouch (body : Action): void {
+        input.onPinPressed(TouchPin.P0, body)
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% blockId=device_foldio_rightEar block="On right Ear touch" 
+    //%blockExternalInputs=false
+    export function rTouch(body: Action): void {
+        input.onPinPressed(TouchPin.P3, body)
+    }
+
+}
