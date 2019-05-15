d221913f6d9ffa3b0f59ebe14527f3fa08f162b9
SongOfNightingale <crawling_91@mail.ru>
2019-05-15 17:34:43 +0200

    Update foldio.ts

--- foldio.ts
+++ foldio.ts
@@ -1,4 +1,12 @@
 namespace foldio {
+      /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% blockId=device_foldio_onPinPressed block="On Pin Pressed"
+    //% shim=foldio::opp
     export function onPinPressed(pinId: number, handler: RefAction) {
         let pin = getPin(pinId);
         if (!pin) return;
@@ -6,6 +14,14 @@ namespace foldio {
         pxtcore.registerWithDal(pin.id, DAL.MICROBIT_BUTTON_EVT_CLICK, handler);
     }
 
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% blockId=device_foldio_onPinReleased block="On Pin Released" 
+    //% shim=foldio::opr
     export function onPinReleased(pinId: number, handler: RefAction) {
         let pin = getPin(pinId);
         if (!pin) return;
@@ -13,6 +29,14 @@ namespace foldio {
         pxtcore.registerWithDal(pin.id, DAL.MICROBIT_BUTTON_EVT_UP, handler);
     }
 
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% blockId=device_foldio_pinIsPressed block="Pin is Pressed" 
+    //% shim=foldio::pip
     export function pinIsPressed(pinId: number): boolean {
         let pin = getPin(pinId);
         if (!pin) return false;
