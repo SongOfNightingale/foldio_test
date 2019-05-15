6e232fbb8cba05ee5666f678334a505e0795b73c
SongOfNightingale <crawling_91@mail.ru>
2019-05-15 15:51:47 +0200

    Update foldio.ts

--- foldio.ts
+++ foldio.ts
@@ -1,24 +1,21 @@
 namespace foldio {
-    /**
-	* Control of the Foldio animal.
-	*/
-	//% Id=device_no_blockID block="my block"  icon="\uF006" color=#e59920 weight=97
-    /**
-     * Do something when a pin is released.
-     * @param name the pin that needs to be released, eg: TouchPin.P0
-     * @param body the code to run when the pin is released
-     */
-    //% help=input/on-pin-released weight=6 blockGap=8
-    //% blockId=device_ear_touched block="on Ear %NAME|Touched"
-	export function onEarPressed(name : TouchPin, body : Action) {
-        let pin = getPin(name);
+    export function onPinPressed(pinId: number, handler: RefAction) {
+        let pin = getPin(pinId);
         if (!pin) return;
-
-        // Forces the PIN to switch to makey-makey style detection.
         pin.isTouched();
-        pxtcore.registerWithDal(name, MICROBIT_BUTTON_EVT_CLICK, body);
+        pxtcore.registerWithDal(pin.id, DAL.MICROBIT_BUTTON_EVT_CLICK, handler);
     }
 
-}
+    export function onPinReleased(pinId: number, handler: RefAction) {
+        let pin = getPin(pinId);
+        if (!pin) return;
+        pin.isTouched();
+        pxtcore.registerWithDal(pin.id, DAL.MICROBIT_BUTTON_EVT_UP, handler);
+    }
 
+    export function pinIsPressed(pinId: number): boolean {
+        let pin = getPin(pinId);
+        if (!pin) return false;
+        return pin.isTouched();
+    }
 }
